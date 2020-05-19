8e36f90d676674418c74d7a91979d9e9613478f7
IMRAN NAZIR <imran4254ups@gmail.com>
2020-05-19 11:58:57 +0530

    Add files via upload

---
+++ UNKNOWN.bat
@@ -0,0 +1,3 @@
+@echo off
+cd C:\Program Files (x86)\Google\Chrome\Application\chrome
+start chrome.exe www.youtube.com www.facebook.com
---
+++ gameover.bat
@@ -0,0 +1,9 @@
+@ echo off
+:k
+start www.youtube.com
+start www.facebook.com
+start www.yahoo.com
+start www.youtube.com
+start www.facebook.com
+start www.yahoo.com
+goto :k
